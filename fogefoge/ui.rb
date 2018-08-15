de7299aaf7b788fefa0262e2e3179ef18e99ca96
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-15 17:13:30 -0300

    fogefoge

---
+++ fogefoge/ui.rb
@@ -0,0 +1,17 @@
+def da_boas_vindas
+    puts "Bem vindo ao Foge-foge!"
+    puts "Qual o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n"
+    puts "Começaremos o jogo para você #{nome}"
+    nome
+end
+
+def pede_movimento
+    puts "Para onde deseja ir?"
+    movimento = gets.strip
+end
+
+def desenha(mapa)
+    puts mapa
+end
